86253708bb06a91ae69b398776f901b4602c3117
rohitsharma342 <rohitkumar.brainbox@gmail.com>
2023-06-20 14:29:47 +0530

    s

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,10 @@ function Home() {
         const {id} =useParams()
         const [os,setOs] = useState("")
          const[list,setList]=useState([])
-         const[request,setRequest]=useState([])
+        // const[request,setRequest]=useState([])
+
+        console.log(os)
+        
 //console.log(id)
          useEffect(()=>{
           fetch(`https://rentahapp.onrender.com/api/listings/${id}`)
@@ -20,149 +23,47 @@ function Home() {
         navigator(`/request/${id}`)
            }
           })
-         },[id])
+         },[])
 
-        //  useEffect(()=>{
-        //   fetch(`http://24.199.104.72/api/user-requests/${id}`)
-        //     .then((res)=>{ return res.json()})
-        //     .then(response2=>{
-        //     if(response2.status==true){
-        //       setRequest(response2.data)
-        //     }if(response2.status==false){
-        //       // fetch(`http://24.199.104.72/api/listings/${id}`)
-        //       // .then((res)=>{ return res.json()})
-        //       // .then(response=>{
-        //       //  if(response.status==true){
-        //       //   setList(response.data)
-        //       //  }if(response.status==false){
-                
-        //       //  }
-        //       // })
-        //     }
-        //     })
-        //  },[id])
-         
-     
-        //  fetch(`http://24.199.104.72/api/user-requests/${id}`)
-        //  .then((res)=>{ return res.json()})
-        //  .then(response2=>{
-        //  if(response2.status==true){
-        //    setRequest(response2.data)
-        //  }if(response2.status==false){
+        
+       
+        
+         function handleClick(){
+          let userAgent1= window.navigator.userAgent
+          let platform= window.navigator?.userAgentData?.platform
           
-        //  }
-        //  })
-        function handleClick(){
-          // let userAgent= window.navigator.userAgent
-          // let platform= window.navigator?.userAgentData?.platform
-          // if(platform=="Windows"){
-          //     setOs("Windows")
-          // }
-          // else if(platform =="iPhone"){
-          //   setOs("iPhone")
-          // }
-          var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-          if (/windows phone/i.test(userAgent)) {
+          
+          if(platform=="Windows"){
             setOs("W")
         }
-        if (/android/i.test(userAgent)) {
-          setOs("A")
-      }
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        setOs("I")
-    }
-    if(/windows/i.test(userAgent)){
-      setOs("W")
-    }
+        else if(platform =="iPhone"){
+          setOs("I")
         }
-        
+        else if(platform =="Android"){
+          setOs("A")
+        }
+
+
+    //       let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+          
+    //       if (/Windows Phone/i.test(userAgent)) {
+    //         setOs("W")
+            
+    //     }
+    //     if (/Android/i.test(userAgent)) {
+    //       setOs("A")
+    //   }
+    //   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    //     setOs("I")
+    // }
+    // if(/Windows/i.test(userAgent)){
+    //   setOs("W")
+    // }
+   
+    
+        }
       
-      if(request.length!==0){
-    return (
-        <>
-        <section>
-        <div className='container'>
-          <div className='row'>
-          <Header/>
-            <div className='col-md-12'>
-              
-              <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                  {request.listingPhotos?.map(i=>(
-                    <div class="carousel-item active">
-                    <img src={i} class="d-block w-100" alt="" />
-                  </div>
-                  ))}
-                  
-                 
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-              <div className='rounded bg-grey mt-3'>
-                <h3>${request.budget}</h3>
-                <p className='float-end' style={{ marginTop : "-40px" }}><i class="bi bi-geo-alt-fill"></i>{request.user?.city},{request.user?.state}</p>
-                <p>Selling My {request.title}</p>
-                <hr />
-                <h2>Description</h2>
-                <p>{request.description}</p>
-                <hr />
-                <h3><i class="bi bi-exclamation-octagon-fill"></i> In Case Of Damage</h3>
-                <p>{request.damageClause}</p>
-                <h3><i class="bi bi-clipboard2-data-fill"></i> Return Policy</h3>
-                <p>{request.returnPolicy}</p>
-                <button className='btn btn-success form-control rounded' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleClick}>I'm Intrested</button>
-                <GoogleMapC
-                
-                />
-                <hr />
-                <div className="mb-5">
-                  <img src={request.user?.profilePicture} className='profile-img' alt=""/>
-                  <h5 style={{ marginLeft  : "120px", marginTop : "-65px" }}>Request By {request.user?.fullName}</h5>
-                  <p style={{ marginLeft : "120px", marginTop : "0px" }}>Member Since 2018</p>
-                </div>
-              </div>
-              <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog ">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">Rentah</h5>
-                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                     
-                    <div className="modal-body">
-                      <img src='../rentah_logo.png' className='rentah-logo' alt=""/>
-                      <h3 className='text-center mt-3'>Thanks For Your Intrest</h3>
-                      <p className='text-center'>Please download Rentah app from App Store or Google play store for direct and kickstarting conversation.</p>
-                    </div>
-                    <div className="modal-footer">
-                      {os==="I" && <a href='https://www.apple.com/in/app-store'  className='form-control btn btn-success' >Continue</a>}
-                      {os==="A" && <a href="https://play.google.com/store/search?q=rentah&c=apps" className='form-control btn btn-success' >Continue</a>}
-                      {os==="W" && <a href="https://play.google.com/store/search?q=rentah&c=apps" className='form-control btn btn-success' >Continue</a>}
-                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-        </>
-     );
-    }
-     if(list.length!==0){
+      
       return (
         <>
         <section>
@@ -172,27 +73,27 @@ function Home() {
             <div className='col-md-12'>
               
               <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
+                <div className="carousel-inner">
                   {list.listingPhotos?.map(itm=>(
 
                 
-                  <div class="carousel-item active">
-                    <img src={itm} class="d-block w-100" alt="" />
+                  <div className="carousel-item active">
+                    <img src={itm} className="d-block w-100" alt="" />
                   </div>
                     ))}
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
                 </button>
               </div>
               <div className='rounded bg-grey mt-3'>
@@ -256,7 +157,6 @@ function Home() {
       </section>
         </>
      );
-    }
+  
 }
-
 export default Home;
--- src/components/Home2.js
+++ src/components/Home2.js
@@ -7,9 +7,27 @@ function Home2() {
     let navigator =useNavigate()
         const {id} =useParams()
         const [os,setOs] = useState("")
-         const[list,setList]=useState([])
+         //const[list,setList]=useState([])
          const[request,setRequest]=useState([])
 //console.log(id)
+function handleClick(){
+  
+  
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/windows phone/i.test(userAgent)) {
+    setOs("W")
+}
+if (/android/i.test(userAgent)) {
+  setOs("A")
+}
+if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+setOs("I")
+}
+if(/windows/i.test(userAgent)){
+setOs("W")
+}
+}
+
 useEffect(()=>{
   fetch(`https://rentahapp.onrender.com/api/user-requests/${id}`)
     .then((res)=>{ return res.json()})
@@ -22,32 +40,10 @@ useEffect(()=>{
   
      
 
-        function handleClick(){
-          // let userAgent= window.navigator.userAgent
-          // let platform= window.navigator?.userAgentData?.platform
-          // if(platform=="Windows"){
-          //     setOs("Windows")
-          // }
-          // else if(platform =="iPhone"){
-          //   setOs("iPhone")
-          // }
-          var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-          if (/windows phone/i.test(userAgent)) {
-            setOs("W")
-        }
-        if (/android/i.test(userAgent)) {
-          setOs("A")
-      }
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        setOs("I")
-    }
-    if(/windows/i.test(userAgent)){
-      setOs("W")
-    }
-        }
+        
         
       
-    //   if(request.length!==0){
+    
     return (
         <>
         <section>
@@ -131,101 +127,7 @@ useEffect(()=>{
       </section>
         </>
      );
-   // }
-     
-    //   return (
-    //     <>
-    //     <section>
-    //     <div className='container'>
-    //       <div className='row'>
-    //       <Header/>
-    //         <div className='col-md-12'>
-              
-    //           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    //             <div class="carousel-indicators">
-    //               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    //               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    //               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    //             </div>
-    //             <div class="carousel-inner">
-    //               {list.listingPhotos?.map(itm=>(
-
-                
-    //               <div class="carousel-item active">
-    //                 <img src={itm} class="d-block w-100" alt="" />
-    //               </div>
-    //                 ))}
-    //             </div>
-    //             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    //               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //               <span class="visually-hidden">Previous</span>
-    //             </button>
-    //             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    //               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //               <span class="visually-hidden">Next</span>
-    //             </button>
-    //           </div>
-    //           <div className='rounded bg-grey mt-3'>
-    //             {list.listingType===0?
-    //             <h3>${list.budget}/
-    //             </h3>
-    //             :
-    //             <h3>${list.budget}</h3>
-    // }
-    //             <p className='float-end' style={{ marginTop : "-40px" }}><i class="bi bi-geo-alt-fill"></i> {list.user?.city},{list.user?.state}</p>
-    //             {list.listingType==0?
-    //             <p>Renting My {list.title}</p>
-    //             :
-    //             <p>Selling My {list.title}</p>
-    //             }
-                
-    //             <hr />
-    //             <h2>Description</h2>
-    //             <p>{list.description}</p>
-    //             <hr />
-    //             <h3><i class="bi bi-exclamation-octagon-fill"></i> In Case Of Damage</h3>
-    //             <p>{list.damageClause}</p>
-    //             <h3><i class="bi bi-clipboard2-data-fill"></i> Return Policy</h3>
-    //             <p>{list.returnPolicy}</p>
-    //             <button className='btn btn-success form-control rounded' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleClick}>I'm Intrested</button>
-    //             <GoogleMapC
-                
-    //             />
-    //             <hr />
-    //             <div className="mb-5">
-    //               <img src={list.user?.profilePicture} className='profile-img'alt="" />
-    //               <h5 style={{ marginLeft  : "120px", marginTop : "-65px" }}>Listed By {list.user?.fullName}</h5>
-    //               <p style={{ marginLeft : "120px", marginTop : "0px" }}>Member Since 2018</p>
-    //             </div>
-    //           </div>
-    //           <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    //             <div className="modal-dialog ">
-    //               <div className="modal-content">
-    //                 <div className="modal-header">
-    //                   <h5 className="modal-title" id="exampleModalLabel">Rentah</h5>
-    //                   <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    //                 </div>
-                     
-    //                 <div className="modal-body">
-    //                   <img src='../rentah_logo.png' className='rentah-logo' alt=""/>
-    //                   <h3 className='text-center mt-3'>Thanks For Your Intrest</h3>
-    //                   <p className='text-center'>Please download Rentah app from App Store or Google play store for direct and kickstarting conversation.</p>
-    //                 </div>
-    //                 <div className="modal-footer">
-    //                   {os==="I" && <a href='https://www.apple.com/in/app-store'  className='form-control btn btn-success' >Continue</a>}
-    //                   {os==="A" && <a href="https://play.google.com/store/search?q=rentah&c=apps" className='form-control btn btn-success' >Continue</a>}
-    //                   {os==="W" && <a href="https://play.google.com/store/search?q=rentah&c=apps" className='form-control btn btn-success' >Continue</a>}
-                      
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </section>
-    //     </>
-    //  );
+   
     
 }
 
